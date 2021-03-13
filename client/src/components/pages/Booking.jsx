0963f032ba065a9fe4329a1d6d31723723bac799
jkidflash <59710389+jkidflash@users.noreply.github.com>
2021-03-13 13:32:48 -0500

    adding date picker to form

--- client/src/components/pages/Booking.jsx
+++ client/src/components/pages/Booking.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBBtn } from 'mdbreact';
-import { Form, Container } from 'react-bootstrap';
-import {UpdateTokenContext} from '../sessions/TokenContext'
-
+import { MDBCol, MDBBtn } from 'mdbreact';
+import { Form } from 'react-bootstrap';
 import { useState } from 'react';
 import Axios from 'axios';
-import { Redirect } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import DatePicker from "react-datepicker"; // npm install react-datepicker (reference: https://www.npmjs.com/package/react-datepicker)
+import "react-datepicker/dist/react-datepicker.css"; // npm install react-datepicker
+
+
 
 const Booking = () => {
     const [inputs, setInputs] = useState({
@@ -24,11 +25,19 @@ const Booking = () => {
     const [redirect, setRedirect] = useState(false);
 
     const handleSubmit = async event => {
-        toast('You clicked submit')
         event.preventDefault();
         try {
             const resp = await Axios.post('/bookings/', inputs);
-            console.log(resp.status);
+            if (resp.status === 200) {
+                toast('You have booked successfully!', {
+                    type: toast.TYPE.SUCCESS
+                });
+                setRedirect(true);
+            } else {
+                toast(resp.status + "There was an issue booking you.", {
+                    type: toast.TYPE.ERROR
+                  });
+            }
         } catch (error) {
             toast("There was an issue booking you.", {
               type: toast.TYPE.ERROR
@@ -47,7 +56,30 @@ const Booking = () => {
         }));
       };
 
+
+    /**
+     * note for code below: process of binding mongodb to selection input type
+     * [ NOT FINISHED ]
+     */
+     // const componentDidMount = () => {
+     //     Axios.get('/merchants/')
+     //     .then((response) => {
+     //         const data = response.data
+     //         console.log(data);
+     //     })
+     // }
+    //componentDidMount()
+      
+    // add date picker variables
+    const [startDate, setStartDate] = useState(new Date());
+    // handle when the day changes
+    const handleDayChange = (date) => {
+        setStartDate(date);
+        inputs.date = JSON.stringify(date);
+    }
+
     return (
+        
         <div className="banner">
             <div className="container">
                 <div className="banner-content">
@@ -60,6 +92,7 @@ const Booking = () => {
                                 Merchant ID
                             </label>
                             <Form.Control onChange={handleInputChange} value={inputs.merchantId} type="text" id="defaultFormRegisterNameEx" className="form-control" name="merchantId"/>
+
                         </Form.Group>
                         <Form.Row>
                             {/**First Name */}
@@ -97,12 +130,12 @@ const Booking = () => {
                         </Form.Group>
                         </Form.Row>
 
-                        {/**Date -> need to be calendar */}
+                        {/**Date */}
                         <Form.Group>
                             <label htmlFor="defaultFormRegisterEmailEx" className="grey-text" name="date">
                                 Date
                             </label>
-                            <Form.Control onChange={handleInputChange} value={inputs.date} type="text" id="defaultFormRegisterEmailEx" className="form-control" name="date"/>
+                            <DatePicker selected={startDate} onChange={handleDayChange} />
                         </Form.Group>
 
                         {/**time -> needs to be in time format*/}
@@ -114,7 +147,7 @@ const Booking = () => {
                         </Form.Group>
 
                         <Form.Row>
-                        {/**Service -> needs to be in time format*/}
+                        {/**Service*/}
                         <Form.Group as={MDBCol} >
                             <label htmlFor="defaultFormRegisterEmailEx" className="grey-text" name="service">
                                 Service
@@ -122,7 +155,7 @@ const Booking = () => {
                             <Form.Control onChange={handleInputChange} value={inputs.service} type="text" id="defaultFormRegisterEmailEx" className="form-control" name="service"/>
                         </Form.Group>
 
-                        {/**Comments -> needs to be in time format*/}
+                        {/**Comments*/}
                         <Form.Group as={MDBCol} >
                             <label htmlFor="defaultFormRegisterEmailEx" className="grey-text" name="comments">
                                 Comments
